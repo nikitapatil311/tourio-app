5fe222fc0c72fab8d58b99447832feaeb3b43359
NAVSmith <56131958+NAVSmith@users.noreply.github.com>
2023-07-13 16:48:07 +0200

    fix a bug with the db connection not routing to the the places collection

--- db/models/places.js
+++ db/models/places.js
@@ -6,7 +6,7 @@ import mongoose, { Schema } from "mongoose";
 const placesSchema = new Schema({
     name: { type: String, required: true },
     location: { type: String, required: true },
-    image: { type: String },
+    image: { type: String , required: true},
     mapURL: { type: String, required: true },
     description: { type: String, required: true },
 });
--- pages/api/places/index.js
+++ pages/api/places/index.js
@@ -1,10 +1,34 @@
-import { places } from '../../../lib/db';
-import dbConnect from '../../../db/connect'
+import Place from "../../../db/models/places";
+import dbConnect from "../../../db/connect";
 
 export default async function handler(request, response) {
-  //connect to the db  
-  await dbConnect()
-  return response.status(200).json(places);
+  console.log('connceting to the db');
+    //connect to the db
+    await dbConnect();
+    // collect data from client
+    const method = request.method;
+    console.log(method);
+
+    // the switch board
+    switch (method) {
+        // when reading the data
+        case "GET":
+          console.log('starting to get the data');
+            console.log(Place);
+            const places = await Place.find();
+            console.log(places);
+            if (places) {
+                return response.status(200).json(places);}
+            // } else {
+            //     return response
+            //         .status(400)
+            //         .json({ status: "No data was found" });
+            // }
+
+            break;
+        // when adding data‚
+        case "POST":
+            console.log("posting");
+            break;
+    }
 }
-
-
